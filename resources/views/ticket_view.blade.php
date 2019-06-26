5f86bd6c80a6f911e04d9c588f6946c36bf4b5f8
Shawn Q <qiushuo@live.com>
2019-06-26 16:29:21 +0800

    add tracking style

--- resources/views/ticket_view.blade.php
+++ resources/views/ticket_view.blade.php
@@ -29,6 +29,140 @@
 #inputPriority .btn:not(.selected) {
     display: none;
 }
+
+
+.tracking-detail {
+ padding:3rem 0
+}
+#tracking {
+ margin-bottom:1rem
+}
+[class*=tracking-status-] p {
+ margin:0;
+ font-size:1.1rem;
+ color:#fff;
+ text-transform:uppercase;
+ text-align:center
+}
+[class*=tracking-status-] {
+ padding:1.6rem 0
+}
+.tracking-status-intransit {
+ background-color:#65aee0
+}
+.tracking-status-outfordelivery {
+ background-color:#f5a551
+}
+.tracking-status-deliveryoffice {
+ background-color:#f7dc6f
+}
+.tracking-status-delivered {
+ background-color:#4cbb87
+}
+.tracking-status-attemptfail {
+ background-color:#b789c7
+}
+.tracking-status-error,.tracking-status-exception {
+ background-color:#d26759
+}
+.tracking-status-expired {
+ background-color:#616e7d
+}
+.tracking-status-pending {
+ background-color:#ccc
+}
+.tracking-status-inforeceived {
+ background-color:#214977
+}
+.tracking-list {
+ border:1px solid #e5e5e5
+}
+.tracking-item {
+ border-left:1px solid #e5e5e5;
+ position:relative;
+ padding:2rem 1.5rem .5rem 2.5rem;
+ font-size:.9rem;
+ margin-left:3rem;
+ min-height:5rem
+}
+.tracking-item:last-child {
+ padding-bottom:4rem
+}
+.tracking-item .tracking-date {
+ margin-bottom:.5rem
+}
+.tracking-item .tracking-date span {
+ color:#888;
+ font-size:85%;
+ padding-left:.4rem
+}
+.tracking-item .tracking-content {
+ padding:.5rem .8rem;
+ background-color:#f4f4f4;
+ border-radius:.5rem
+}
+.tracking-item .tracking-content span {
+ display:block;
+ color:#888;
+ font-size:85%
+}
+.tracking-item .tracking-icon {
+ line-height:2.6rem;
+ position:absolute;
+ left:-1.3rem;
+ width:2.6rem;
+ height:2.6rem;
+ text-align:center;
+ border-radius:50%;
+ font-size:1.1rem;
+ background-color:#fff;
+ color:#fff
+}
+.tracking-item .tracking-icon.status-sponsored {
+ background-color:#f68
+}
+.tracking-item .tracking-icon.status-delivered {
+ background-color:#4cbb87
+}
+.tracking-item .tracking-icon.status-outfordelivery {
+ background-color:#f5a551
+}
+.tracking-item .tracking-icon.status-deliveryoffice {
+ background-color:#f7dc6f
+}
+.tracking-item .tracking-icon.status-attemptfail {
+ background-color:#b789c7
+}
+.tracking-item .tracking-icon.status-exception {
+ background-color:#d26759
+}
+.tracking-item .tracking-icon.status-inforeceived {
+ background-color:#214977
+}
+.tracking-item .tracking-icon.status-intransit {
+ color:#e5e5e5;
+ border:1px solid #e5e5e5;
+ font-size:.6rem
+}
+@media(min-width:992px) {
+ .tracking-item {
+  margin-left:10rem
+ }
+ .tracking-item .tracking-date {
+  position:absolute;
+  left:-10rem;
+  width:7.5rem;
+  text-align:right
+ }
+ .tracking-item .tracking-date span {
+  display:block
+ }
+ .tracking-item .tracking-content {
+  padding:0;
+  background-color:transparent
+ }
+}
+
 </style>
 
 <form name='ticket' method="POST" action="/ticket/{{$ticket->id}}" >
@@ -58,6 +192,16 @@
                     <label class="col-sm-2 col-form-label">报单人</label>
                     <div class="input-group col-sm-10">
                         <input type="text" class="form-control" value="宋老师" readonly>
+                        <div class="input-group-append">
+                            <button class="btn btn-info" type="button"><i class="fas fa-phone"></i></button>
+                        </div>
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <hr />
+                    <label class="col-sm-2 col-form-label">派单人</label>
+                    <div class="input-group col-sm-10">
+                        <input type="text" class="form-control" value="王经理" readonly>
                         <div class="input-group-append">
                             <button class="btn btn-info" type="button"><i class="fas fa-phone"></i></button>
                         </div>
@@ -193,7 +337,57 @@
             </div>
             <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordionExample">
             <div class="card-body">
-                Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+            <div class="tracking-list">
+               <div class="tracking-item">
+                  <div class="tracking-icon status-intransit">
+                    <i class="fas fa-circle"></i>
+                  </div>
+                  <div class="tracking-date">2019年6月13日<span>05:01 PM</span></div>
+                  <div class="tracking-content">王老师 创建工单<span>101教室照明问题</span></div>
+               </div>
+               <div class="tracking-item">
+                  <div class="tracking-icon status-intransit">
+                    <i class="fas fa-circle"></i>
+                  </div>
+                  <div class="tracking-date">2019年6月13日<span>05:29 PM</span></div>
+                  <div class="tracking-content">赵主任 审核通过<span>请尽快维修!</span></div>
+               </div>
+               <div class="tracking-item">
+                  <div class="tracking-icon status-intransit">
+                    <i class="fas fa-circle"></i>
+                  </div>
+                  <div class="tracking-date">2019年6月13日<span>05:34 PM</span></div>
+                  <div class="tracking-content">王经理 分派工单<span>我已就近分配维修人员</span></div>
+               </div>
+               <div class="tracking-item">
+                  <div class="tracking-icon status-intransit">
+                    <i class="fas fa-circle"></i>
+                  </div>
+                  <div class="tracking-date">2019年6月13日<span>05:40 PM</span></div>
+                  <div class="tracking-content">张师傅 接受工单<span>预计6月13日7:00PM上门</span></div>
+               </div>
+               <div class="tracking-item">
+                  <div class="tracking-icon status-inforeceived">
+                    <i class="fas fa-clipboard-list"></i>
+                  </div>
+                  <div class="tracking-date">2019年6月13日<span>07:42 PM</span></div>
+                  <div class="tracking-content">王经理 调整金额<span>线路开关损坏，更换开关面板。调整工单费用为50元</span></div>
+               </div>
+               <div class="tracking-item">
+                  <div class="tracking-icon status-outfordelivery">
+                      <i class="fas fa-shipping-fast"></i>
+                  </div>
+                  <div class="tracking-date">2019年6月13日<span>07:46 PM</span></div>
+                  <div class="tracking-content">张师傅 未完工，再次预约<span>配件不全，需库房调货。预计6月14日8:30AM上门</span></div>
+               </div>
+               <div class="tracking-item">
+                  <div class="tracking-icon status-outfordelivery">
+                      <i class="fas fa-plus"></i>
+                  </div>
+                  <div class="tracking-date">点此添加记录<span></span></div>
+               </div>
+            </div>
+
             </div>
             </div>
         </div>
