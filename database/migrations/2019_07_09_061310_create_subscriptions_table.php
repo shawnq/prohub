b688f3c1110709b5be8a900034100af7f56aba17
Shawn Q <qiushuo@live.com>
2019-07-09 14:15:23 +0800

    add subscription and location model/controller

--- database/migrations/2019_07_09_061310_create_subscriptions_table.php
+++ database/migrations/2019_07_09_061310_create_subscriptions_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateLocationsTable extends Migration
+class CreateSubscriptionsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,9 @@ class CreateLocationsTable extends Migration
      */
     public function up()
     {
-        Schema::create('locations', function (Blueprint $table) {
+        Schema::create('subscriptions', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->timestamps();
-            $table->string('label');
-            $table->string('path');
-            $table->bigInteger('parent_id');
         });
     }
 
@@ -29,6 +26,6 @@ class CreateLocationsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('locations');
+        Schema::dropIfExists('subscriptions');
     }
 }
